9d30e275ca6872046e6081be19b2e471e98c545a
Janna Fox <jannafox@gmail.com>
2020-07-30 21:37:38 -0600

    MVP

--- index.js
+++ index.js
@@ -109,11 +109,13 @@ Hint: You can use .splice() for this
 */
 
 function removeFlavorByName(array, flavor){
-    array.splice(1)
-
+    const index = array.indexOf(flavor)
+    array.splice(index,1)
+    return array
 }
-console.log(removeFlavorByName(originalFlavors, "Vanilla"))
-originalFlavors.findIndex
+originalFlavors = removeFlavorByName(originalFlavors, "Vanilla")
+console.log(originalFlavors)
+
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
 
 Your function should accept: 
@@ -122,11 +124,13 @@ Your function should accept:
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
 
-function copy(/*code here*/){
+function copy(newArray, originalArray){
 
-    /*code here*/
-
+    newArray = [...originalArray]
+    return newArray;
 }
+console.log(copy("Fun Flavors", originalFlavors))
+
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
@@ -143,11 +147,16 @@ DO NOT USE ADVANCED ARRAY METHODS (i.e. .filter) to solve this problem.
 
 hint - you can use the .includes method to help you solve this */
 
-function filterByWord(/*code here*/){
-
-    /*code here*/
-
+function filterByWord(array, flavor){
+    let filteredArray = [];
+    for (let i=0; i < array.length; i++){
+        if (array[i].includes(flavor)){
+            filteredArray.push(array[i]);
+        }
+    }
+    console.log(filteredArray);
 }
+filterByWord(originalFlavors, 'Chocolate');
 
 
 
